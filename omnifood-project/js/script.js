fe9aad19bcd068bb0f03ab5ef35663efb61f3f2f
Diego Bezerra <diegobmartins21@gmail.com>
2025-05-01 08:30:21 -0300

    applied some js features - Menu, scroll behavior | Applied Accessibility improvements | favicon applied

---
+++ omnifood-project/js/script.js
@@ -0,0 +1,51 @@
+//setting menu navigation behavior
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+//set smooth scroll beahvior
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // scroll to top
+    if (href === "#") window.scrollTo({ top: 0, behavior: "smooth" });
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+//set sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-90px",
+  }
+);
+
+obs.observe(sectionHeroEl);
+
+// Set dynamic year content
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
